4f8e67e927c9190831048fb03bb19ffab519179a
mischb <mischarberlin@gmail.com>
2018-12-22 22:59:43 -0500

    added psql

--- config.js
+++ config.js
@@ -2,15 +2,10 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000' || 'http://localhost:3001',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+      process.env.DATABASE_URL || 'postgres://localhost/petful',
   TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
-  // TEST_DATABASE_URL:
-  //     process.env.TEST_DATABASE_URL ||
-  //     'postgres://localhost/thinkful-backend-test'
+      process.env.TEST_DATABASE_URL ||
+      'postgres://localhost/petful-test'
 };
