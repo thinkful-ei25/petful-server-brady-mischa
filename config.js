0999c75979b9679d2613689a2fdb391c3e4cac3c
mischb <mischarberlin@gmail.com>
2018-12-22 23:01:17 -0500

    removed psql will add to branch

--- config.js
+++ config.js
@@ -2,10 +2,15 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000' || 'http://localhost:3001',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-      process.env.DATABASE_URL || 'postgres://localhost/petful',
+        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
   TEST_DATABASE_URL:
-      process.env.TEST_DATABASE_URL ||
-      'postgres://localhost/petful-test'
+        process.env.TEST_DATABASE_URL ||
+        'mongodb://localhost/thinkful-backend-test'
+  // DATABASE_URL:
+  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
+  // TEST_DATABASE_URL:
+  //     process.env.TEST_DATABASE_URL ||
+  //     'postgres://localhost/thinkful-backend-test'
 };
--- routes/pets.js
+++ routes/pets.js
@@ -1,87 +1,62 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-// const dogs = require('../queues/dogs');
-// const cats = require('../queues/cats');
-const knex = require('../knex');
+const dogs = require('../queues/dogs');
+const cats = require('../queues/cats');
 
 /* ================ GET ================ */
 
-function selectOnePetFromDb(req,res,next){
+router.get('/pets/:pets', (req, res, next) => {
   let petType = req.params.pets;
-  if(petType !== 'dogs' && petType !== 'cats'){
-    const err = new Error;
+  if (petType === 'cats') {
+    if (cats.peek()) {
+      res.json(cats.peek());
+    } else {
+      res.json();
+    }
+  } else if (petType === 'dogs') {
+    if (dogs.peek()) {
+      res.json(dogs.peek());
+    } else {
+      res.json();
+    }
+  } else {
+    const err = new Error(
+      'Sorry, cats and dogs only! Check back soon for my animals!'
+    );
     err.status = 404;
-    err.message = ('Sorry, cats and dogs only! Check back soon for my animals!');
     next(err);
   }
-  knex(petType).select().limit(1)
-    .then(([pet]) => {
-      if(pet){
-        req.pet = pet;
-      }else{
-        req.pet = null;
-      }
-      next();
-    })
-    .catch(err =>  next(err));
-}
-router.get('/pets/:pets', selectOnePetFromDb, (req, res) => {
-  console.log('get request:', req.pet);
-  if(req.pet){
-    res.json(req.pet);
-  }else{
-    res.sendStatus(200);
-  }
 });
-/*--- for queing without db ---*/
 
-// if(petType === 'cats') {
-//   res.json(cats.peek());
-// } else if(petType === 'dogs') {
-//   res.json(dogs.peek());
-// } else {
-//   const err = new Error('Sorry, cats and dogs only! Check back soon for my animals!');
-//   err.status = 404;
-//   next(err);
-  
-// }
-
-router.delete('/pets/:pets', selectOnePetFromDb, (req, res, next) => {
+router.delete('/pets/:pets', (req, res, next) => {
   let petType = req.params.pets;
-  if(req.pet){
-    knex(petType).del().where('id', req.pet.id)
-      .then(() => res.sendStatus(204))
-      .catch(err => next(err));
-  }else{
-    res.sendStatus(204);
+  let err;
+  if (petType === 'cats') {
+    if (cats.peek()) {
+      cats.dequeue();
+      res.sendStatus(204);
+    } else {
+      err = new Error('Sorry, we currently have no cats up for adoption');
+      err.status = 404;
+      next(err);
+    }
+  } else if (petType === 'dogs') {
+    if (dogs.peek()) {
+      dogs.dequeue();
+      res.sendStatus(204);
+    } else {
+      err = new Error('Sorry, we currently have no dogs up for adoption');
+      err.status = 404;
+      next(err);
+    }
+  } else {
+    err = new Error(
+      'Sorry, cats and dogs only! Check back soon for my animals!'
+    );
+    err.status = 404;
+    next(err);
   }
 });
-//for queing without db
-// let err;
-// if (petType === 'cats') {
-//   if(cats.peek()){
-//     cats.dequeue();
-//     res.sendStatus(204);
-//   } else {
-//     err = new Error('Sorry, we currently have no cats up for adoption');
-//     err.status = 404;
-//     next(err);
-//   }
-// } else if (petType === 'dogs') {
-//   if (dogs.peek()) {
-//     dogs.dequeue();
-//     res.sendStatus(204);
-//   } else {
-//     err = new Error('Sorry, we currently have no dogs up for adoption');
-//     err.status = 404;
-//     next(err);
-//   }
-// } else {
-//   err = new Error('Sorry, cats and dogs only! Check back soon for my animals!');
-//   err.status = 404;
-//   next(err);
-// }
-
 
 module.exports = router;
