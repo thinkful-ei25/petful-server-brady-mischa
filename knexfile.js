da1065cd0d2a54048abebca9d9acd77b0713afdb
mischb <mischarberlin@gmail.com>
2018-12-24 10:55:29 -0500

    updated knexfile for heroku

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,11 @@ module.exports = {
     pool: { min: 1, max: 2}
   },
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL
+    client: 'postgresql',
+    connection: {
+      database: 'postgres://bqlliyzhdglqcs:280a14c61c369f74238052e0806058a9e07810cb5c824acad62f4d03bdec3324@ec2-54-225-150-216.compute-1.amazonaws.com:5432/d9332dr801j9bs',
+      user:     'username',
+      password: 'password'
+    }
   }
 };
